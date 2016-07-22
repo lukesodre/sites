5b46c22a5f77a8054f77d07f03ffd0351ff221fb
Lucas Sodre <lucasasodre@gmail.com>
2016-07-21 22:34:32 -0300

    add site monica

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+  app = express(),
+  http = require('http'),
+  httpServer = http.Server(app);
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());  
+  
+app.use(express.static(__dirname));
+
+app.post('/', function(req, res) {
+  console.log("Transacao efetuada")
+  var transacao = {}
+  transacao.status = "Sucesso"
+  transacao.body = req.body;
+  res.json(transacao);
+});
+
+
+app.listen(3000);
+console.log("Escutando porta 3000")
